718af8f99dc79cb5aba9dac5da656805aa8d80f3
LarioDiniz <lario.diniz@palo.com.br>
2018-04-25 14:14:36 -0300

    Aula 04
    Aula 04

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,19 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FotoModule } from './foto/foto.module';
+import { PainelModule } from './painel/painel.module';
 import { HttpModule } from '@angular/http';
 
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { ListagemComponent } from './listagem/listagem.component';
+
+import { routing } from './app.routes';
 
 import 'rxjs/add/operator/map';
 
 @NgModule({
     imports: [
         BrowserModule, 
-        FotoModule,
-        HttpModule
+        HttpModule,
+        routing,
+        FotoModule,        
+        PainelModule
     ],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CadastroComponent,
+        ListagemComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule {
 
